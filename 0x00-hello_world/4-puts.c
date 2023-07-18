964571c751f66e5d14d4ce4a47c5076b4933ac60
Toluwalope <victorioustolu@gmail.com>
2023-07-17 19:57:49 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+*main function
+*Return 0: Success
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multiligual puzzle");
-  return(0);
-    }
+puts("\"Programming is like building a multiligual puzzle");
+return (0);
+}
